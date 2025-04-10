1465dc1e1d83a2bdb5fc01a2cc802680caea89e1
mariacastro11 <maria1.castro.stj@gmail.com>
2025-04-09 21:42:43 -0500

    fix: Change js to ts
    - Create styles folder
    
    - Updated configuration files
    
    - Organized folders

---
+++ src/components/cardViewer.ts
@@ -0,0 +1,67 @@
+class CardViewer extends HTMLElement {
+
+    static get observedAttributes (): string[] {
+        return ['UserProfile', 'UserName', 'Comment', 'SongName', 'Album', 'AlbumCover', 'Artist'];
+    }
+
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});
+    }
+
+    connectedCallback(): void {
+        this.render();
+    }
+
+    attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
+        if(oldValue !== newValue){
+            this.setAttribute(name, newValue);
+            this.render();
+        }
+    }
+
+
+    render(): void {
+        if (this.shadowRoot) { 
+        
+            this.shadowRoot.innerHTML=`
+            <link rel="stylesheet" href="/styles/cardViewer.css">
+
+            <div class="MegaCardContainer">
+                <div class="GlobalCardContainer">
+                    <div class="CardViewer">
+                        <div class="CardHeader">
+                            <img src="${this.getAttribute('UserProfile')}" alt="" class="UserProfile">
+                            <p class="UserName">${this.getAttribute('UserName')}</p>
+                        </div>
+
+                        <p class="Comment">${this.getAttribute('Comment')}</p>
+
+                        <div class="Music">
+                            <div class="Music1">
+                                <div class="Music2">
+                                    <p class="SongName">${this.getAttribute('SongName')}</p>
+                                    <p class="Album">${this.getAttribute('Album')}</p>
+                                </div>
+
+                                <p class="Artist">${this.getAttribute('Artist')}</p>
+                            </div>
+
+                            <img src="${this.getAttribute('AlbumCover')}" alt="" class="AlbumCover">
+                        </div>
+                        <div class="Commentary">
+                            <textarea class="TextCamp1" placeholder="Comment something :/"></textarea>
+                            <a href=""><img src="../../../public/icons/publicar-08.png" alt="" class="IconMessage"></a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            `;
+        }
+    }
+}
+
+customElements.define('card-viewer', CardViewer)
+
+export default CardViewer;
--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export {default as CardComment} from './CardComment/cardComment.js'
-export {default as CardViewer} from './CardViewer/cardViewer.js'
