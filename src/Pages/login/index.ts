62920b01a0d0a4cd42a30bfe5b86e4ca09503a2b
CtrlMagno <jonnathanesmen3@gmail.com>
2025-04-21 16:47:38 -0500

    Connection with pages, New Login, New Sign up

---
+++ src/Pages/login/index.ts
@@ -0,0 +1,5 @@
+import '../../../public/styles/login.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Login page loaded');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,14 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: 'development',
-  entry: "./src/Pages/home/appContainer.ts",
+  entry: {
+    login: "./src/Pages/login/index.ts",
+    signup: "./src/Pages/signup/index.ts",
+    home: "./src/Pages/home/appContainer.ts",
+    landing: "./src/Pages/landing/index.ts",
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
     publicPath: "/"
@@ -19,19 +24,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-            },
-          },
-        ],
-        include: [
-          path.resolve(__dirname, "src"),
-          path.resolve(__dirname, "public"),
-        ],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
@@ -47,21 +40,40 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/Pages/home/home.html",
-      inject: true,
-      filename: 'index.html'
+      template: "./src/Pages/login/index.html",
+      filename: 'index.html',
+      chunks: ['login']
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/Pages/signup/index.html",
+      filename: 'signup',
+      chunks: ['signup']
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/Pages/home/home.html",
+      filename: 'home',
+      chunks: ['home']
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/Pages/landing/index.html",
+      filename: 'landing',
+      chunks: ['landing']
+    })
   ],
   devServer: {
     static: {
       directory: path.join(__dirname, "public"),
-      publicPath: "/"
     },
     hot: true,
     port: 8080,
-    historyApiFallback: true,
-    devMiddleware: {
-      publicPath: "/"
-    }
+    historyApiFallback: {
+      rewrites: [
+        { from: /^\/signup$/, to: '/signup.html' },
+        { from: /^\/home$/, to: '/home.html' },
+        { from: /^\/landing$/, to: '/landing.html' },
+        { from: /./, to: '/index.html' }
+      ]
+    },
+    open: true
   },
-};
+}
